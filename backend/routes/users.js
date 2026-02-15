c7dc8895bc88688e995fd5a57053d30ceb93b1e3
Samm005 <samudyathakaranth@gmail.com>
2026-02-15 21:14:42 +0530

    Adding jwt verification logic

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { update,Delete,likeVideo,dislikeVideo,getUser } from "../controllers/userController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 //Update a user
-router.put("/:id",update);
+router.put("/:id",verifyToken,update);
 
 //Delete a user
 router.delete("/:id",Delete);
