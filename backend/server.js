4d9064fd3000b15bc2829ea816aab9c08f0610d5
Samm005 <samudyathakaranth@gmail.com>
2026-02-15 12:10:26 +0530

    A basic testing function to check user route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connect from "./config/db.js";
+import userRoutes from "./routes/users.js";
 
 dotenv.config();
 
 const app = express();
 
 connect();
+
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}!!`);
