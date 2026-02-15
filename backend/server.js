6b11492cf9ad9708bcc5e894b20cac3562bd8734
Samm005 <samudyathakaranth@gmail.com>
2026-02-15 12:25:55 +0530

    Basic structure for routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import connect from "./config/db.js";
 import userRoutes from "./routes/users.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import channelRoutes from "./routes/channelRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
 
 dotenv.config();
 
@@ -10,6 +13,9 @@ const app = express();
 connect();
 
 app.use("/api/users", userRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/channels", channelRoutes);
+app.use("/api/videos", videoRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}!!`);
